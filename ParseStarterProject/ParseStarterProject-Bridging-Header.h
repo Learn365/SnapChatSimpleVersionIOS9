806ee171293fe692eaab66ef2f74bfc7f8c1c57e
tangweichang <tangweichangjlu@gmail.com>
2015-09-22 01:54:56 -0400

    add the message alert from sender and delete it after 5 secs and the system worked

--- ParseStarterProject/ParseStarterProject-Bridging-Header.h
+++ ParseStarterProject/ParseStarterProject-Bridging-Header.h
@@ -7,6 +7,7 @@
 #ifndef ParseStarterProject_Bridging_Header_h
 #define ParseStarterProject_Bridging_Header_h
 #import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
 
 // If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
 //#import <ParseFacebookUtils/PFFacebookUtils.h>
--- ParseStarterProject/UserTableViewController.swift
+++ ParseStarterProject/UserTableViewController.swift
@@ -14,9 +14,65 @@ class UserTableViewController: UITableViewController, UINavigationControllerDele
     var usernames = [String]()
     var recipientUsername = ""
     
+    func checkForMessage() {
+        //print("checked")
+        if PFUser.currentUser()?.username != nil {
+            var query = PFQuery(className:"image")
+            query.whereKey("recipientUsername", equalTo: (PFUser.currentUser()?.username)!)
+            var images = query.findObjects()
+            
+            if let pfObjects = images as? [PFObject] {
+                if pfObjects.count > 0 {
+                    
+                    var imageView: PFImageView = PFImageView()
+                    imageView.file = pfObjects[0]["photo"] as! PFFile
+                    imageView.loadInBackground({
+                        (photo, error) -> Void in
+                        if error == nil {
+                            var senderUsername = "Unknown User"
+                            if let username = pfObjects[0]["senderUsername"] as? String {
+                                senderUsername = username
+                                
+                            }
+                            var alert = UIAlertController(title: "You have a message", message: "Message from " + senderUsername, preferredStyle: UIAlertControllerStyle.Alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                                
+                                // fade out the background
+                                let backgroundView = UIImageView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
+                                backgroundView.backgroundColor = UIColor.blackColor()
+                                backgroundView.alpha = 0.8
+                                backgroundView.tag = 10
+                                backgroundView.contentMode = UIViewContentMode.ScaleAspectFit
+                                self.view.addSubview(backgroundView)
+                                
+                                
+                                let displayedImage = UIImageView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
+                                displayedImage.image = photo
+                                displayedImage.tag = 10
+                                displayedImage.contentMode = UIViewContentMode.ScaleAspectFit
+                                self.view.addSubview(displayedImage)
+                                
+                                pfObjects[0].delete()
+                                
+                                _ = NSTimer.scheduledTimerWithTimeInterval(4, target: self, selector: Selector("hideMessage"), userInfo: nil, repeats: false)
+                                
+                            }))
+                            self.presentViewController(alert, animated: true, completion: nil)
+                        }
+                        }
+                    )
+                }
+                
+            }
+
+            
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: Selector("checkForMessage"), userInfo: nil, repeats: true)
         
         // Do not have the fancy ways of error handling
         var query = PFUser.query()!
@@ -28,6 +84,15 @@ class UserTableViewController: UITableViewController, UINavigationControllerDele
         
         tableView.reloadData()
 
+    }
+    
+    func hideMessage() {
+        //remove images by using tag number 10
+        for subview in self.view.subviews {
+            if subview.tag == 10 {
+                subview.removeFromSuperview()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,6 +137,10 @@ class UserTableViewController: UITableViewController, UINavigationControllerDele
         imageToSend["photo"] = PFFile(name: "photo.jpg", data: UIImageJPEGRepresentation(image, 0.5)!)
         imageToSend["senderUsername"] = PFUser.currentUser()?.username
         imageToSend["recipientUsername"] = recipientUsername
+        let acl = PFACL()
+        acl.setPublicReadAccess(true)
+        acl.setPublicWriteAccess(true)
+        imageToSend.ACL = acl
         imageToSend.save()
     }
     
